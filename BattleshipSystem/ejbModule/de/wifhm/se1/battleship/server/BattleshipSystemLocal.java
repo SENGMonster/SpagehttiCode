1772ae3506f49a6c2cb28aac46f619317463618c
SENGMonster <buystuff@ramonaleenings.com>
2012-06-15 14:21:20 +0200

    Schiffe positionieren v2

---
+++ BattleshipSystem/ejbModule/de/wifhm/se1/battleship/server/BattleshipSystemLocal.java
@@ -0,0 +1,11 @@
+package de.wifhm.se1.battleship.server;
+
+import javax.ejb.Local;
+
+import de.wifhm.se1.battleship.common.BattleshipSystem;
+
+
+@Local
+public interface BattleshipSystemLocal extends BattleshipSystem {
+
+}
