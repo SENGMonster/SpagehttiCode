fa27139558c0b0db6dc90de9257d66756301a8b6
Holger Tenbeitel <tenbeitelholger@googlemail.com>
2012-06-19 09:37:13 +0200

    added new Preference Screen to Apüp

--- BattleshipApplication/gen/de/wifhm/se1/android/activity/R.java
+++ BattleshipApplication/gen/de/wifhm/se1/android/activity/R.java
@@ -9,13 +9,13 @@ package de.wifhm.se1.android.activity;
 
 public final class R {
     public static final class array {
-        public static final int boardsizes=0x7f060000;
-        public static final int sizeint=0x7f060001;
+        public static final int boardsizes=0x7f070000;
+        public static final int sizeint=0x7f070001;
     }
     public static final class attr {
     }
     public static final class color {
-        public static final int failurebackground=0x7f040000;
+        public static final int failurebackground=0x7f050000;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
@@ -32,39 +32,39 @@ public final class R {
         public static final int wasser=0x7f02000b;
     }
     public static final class id {
-        public static final int boardsize=0x7f080016;
-        public static final int btnPosShipsCancel=0x7f080013;
-        public static final int btnPosShipsOK=0x7f080012;
-        public static final int btnTestAgent=0x7f080001;
-        public static final int btnpositionship=0x7f08000c;
-        public static final int btnspielfeld=0x7f08000b;
-        public static final int exit=0x7f080020;
-        public static final int failuremessage=0x7f080008;
-        public static final int gallery=0x7f08000e;
-        public static final int gotothighscore=0x7f08000d;
-        public static final int gridview=0x7f080000;
-        public static final int gvPosShips=0x7f080014;
-        public static final int highscorelist=0x7f080002;
-        public static final int log=0x7f080009;
-        public static final int login=0x7f080006;
-        public static final int loginPassword=0x7f080005;
-        public static final int loginUsername=0x7f080004;
-        public static final int logout=0x7f08001f;
-        public static final int playgamebtn=0x7f080003;
-        public static final int preferences=0x7f08001e;
-        public static final int reg=0x7f08000a;
-        public static final int registerNew=0x7f080007;
-        public static final int registerPassword=0x7f08001a;
-        public static final int registerPasswordRetype=0x7f08001b;
-        public static final int registerUsername=0x7f080019;
-        public static final int savesettings=0x7f080018;
-        public static final int saveup=0x7f080015;
-        public static final int send=0x7f08001c;
-        public static final int ship_image=0x7f08000f;
-        public static final int shiplength=0x7f080010;
-        public static final int shipname=0x7f080011;
-        public static final int spinner=0x7f080017;
-        public static final int textView1=0x7f08001d;
+        public static final int boardsize=0x7f090016;
+        public static final int btnPosShipsCancel=0x7f090013;
+        public static final int btnPosShipsOK=0x7f090012;
+        public static final int btnTestAgent=0x7f090001;
+        public static final int btnpositionship=0x7f09000c;
+        public static final int btnspielfeld=0x7f09000b;
+        public static final int exit=0x7f090020;
+        public static final int failuremessage=0x7f090008;
+        public static final int gallery=0x7f09000e;
+        public static final int gotothighscore=0x7f09000d;
+        public static final int gridview=0x7f090000;
+        public static final int gvPosShips=0x7f090014;
+        public static final int highscorelist=0x7f090002;
+        public static final int log=0x7f090009;
+        public static final int login=0x7f090006;
+        public static final int loginPassword=0x7f090005;
+        public static final int loginUsername=0x7f090004;
+        public static final int logout=0x7f09001f;
+        public static final int playgamebtn=0x7f090003;
+        public static final int preferences=0x7f09001e;
+        public static final int reg=0x7f09000a;
+        public static final int registerNew=0x7f090007;
+        public static final int registerPassword=0x7f09001a;
+        public static final int registerPasswordRetype=0x7f09001b;
+        public static final int registerUsername=0x7f090019;
+        public static final int savesettings=0x7f090018;
+        public static final int saveup=0x7f090015;
+        public static final int send=0x7f09001c;
+        public static final int ship_image=0x7f09000f;
+        public static final int shiplength=0x7f090010;
+        public static final int shipname=0x7f090011;
+        public static final int spinner=0x7f090017;
+        public static final int textView1=0x7f09001d;
     }
     public static final class layout {
         public static final int gridview=0x7f030000;
@@ -80,24 +80,27 @@ public final class R {
         public static final int succregister=0x7f03000a;
     }
     public static final class menu {
-        public static final int menu=0x7f070000;
+        public static final int menu=0x7f080000;
     }
     public static final class string {
-        public static final int app_name=0x7f050000;
-        public static final int boardsize=0x7f05000b;
-        public static final int choose=0x7f05000d;
-        public static final int exit=0x7f050009;
-        public static final int login=0x7f050005;
-        public static final int logout=0x7f050007;
-        public static final int password=0x7f050003;
-        public static final int passwordretype=0x7f050004;
-        public static final int preferences=0x7f050008;
-        public static final int register=0x7f050002;
-        public static final int registerNew=0x7f050006;
-        public static final int save=0x7f05000c;
-        public static final int savepu=0x7f05000a;
-        public static final int startgame=0x7f05000e;
-        public static final int username=0x7f050001;
+        public static final int app_name=0x7f060000;
+        public static final int boardsize=0x7f06000b;
+        public static final int choose=0x7f06000d;
+        public static final int exit=0x7f060009;
+        public static final int login=0x7f060005;
+        public static final int logout=0x7f060007;
+        public static final int password=0x7f060003;
+        public static final int passwordretype=0x7f060004;
+        public static final int preferences=0x7f060008;
+        public static final int register=0x7f060002;
+        public static final int registerNew=0x7f060006;
+        public static final int save=0x7f06000c;
+        public static final int savepu=0x7f06000a;
+        public static final int startgame=0x7f06000e;
+        public static final int username=0x7f060001;
+    }
+    public static final class xml {
+        public static final int pref=0x7f040000;
     }
     public static final class styleable {
         /** Attributes that can be used with a galleryBorder.
